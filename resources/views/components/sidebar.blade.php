7900903155302f29d8cbc6993b20694b4c3955c8
michelsdev <sinambelamichel05@gmail.com>
2024-06-18 15:36:38 +0700

    modified front end in sidebar

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,5 +1,5 @@
-<div class='flex'>
-<aside id="logo-sidebar" class="top-10 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+<div class='md:flex'>
+<aside id="logo-sidebar" class="top-10 left-0 z-40 w-64 fixed md:relative h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
   <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
      <ul class="space-y-2 font-medium">
         <li>
